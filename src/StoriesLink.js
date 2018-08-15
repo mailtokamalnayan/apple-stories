6d7b5440604446efb919162db1c211f639c9d568
mailtokamalnayan <mailtokamalnayan@gmail.com>
2018-08-16 00:30:27 +0800

    Nail the masonary layout through `columns`

--- src/StoriesLink.js
+++ src/StoriesLink.js
@@ -3,23 +3,25 @@ import React from 'react'
 function StoriesLink(props) {
     const listItems = props.titles.map((title) =>
       <li className="list-item">
-        <h3 className="list-subtitle">{title.subtitle}</h3>
-        <h2 className="list-title">{title.title}</h2>
-          <ul>
-            { title.apps.map(appName => 
-              <li className="app-item">
-                <picture className="app-image">
-                  <source srcSet={appName.picture} media={'(min-width: 1069px)'} />
-                  <img src={appName.avatar} />
-                </picture>
-                <div className="app-info">
-                  <div className="app-title">{appName.title}</div>
-                  <div className="app-category">{appName.category}</div>
-                </div>
-                <a className="app-link" href={appName.link}>VIEW</a>
-              </li> 
-            )} 
-          </ul>
+        <div className="app-item-wraper">
+          <h3 className="list-subtitle">{title.subtitle}</h3>
+          <h2 className="list-title">{title.title}</h2>
+            <ul>
+              { title.apps.map(appName => 
+                <li className="app-item">
+                    <picture className="app-image">
+                      <source srcSet={appName.picture} media={'(min-width: 1069px)'} />
+                      <img src={appName.avatar} />
+                    </picture>
+                    <div className="app-info">
+                      <div className="app-title">{appName.title}</div>
+                      <div className="app-category">{appName.category}</div>
+                    </div>
+                    <a className="app-link" href={appName.link}>VIEW</a>
+                </li> 
+              )} 
+            </ul>
+        </div>
       </li>
     );
     return (
