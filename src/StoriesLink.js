cdb81586063ff00029c2111aecab238b4a4d2b06
mailtokamalnayan <mailtokamalnayan@gmail.com>
2018-08-17 23:17:16 +0800

    Show cards only with links

--- src/StoriesLink.js
+++ src/StoriesLink.js
@@ -3,30 +3,34 @@ import data from './scraped.json'
 
 function StoriesLink(props) {
     const listItems = data.appList.map((title) =>
-      <li className="list-item">
-        <div className="app-item-wraper">
-          <h3 className="list-subtitle">{title.subtitle}</h3>
-          <h2 className="list-title">{title.title}</h2>
-            {title.heroimage && 
-              <div className="list-image"><img src={title.heroimage} /></div>
-            }
-            <ul>
-              { title.apps.map(appName => 
-                <li className="app-item">
-                    <picture className="app-image">
-                      <source srcSet={appName.picture} media={'(min-width: 1069px)'} />
-                      <img src={appName.avatar} />
-                    </picture>
-                    <div className="app-info">
-                      <div className="app-title">{appName.title}</div>
-                      <div className="app-category">{appName.category}</div>
-                    </div>
-                    <a className="app-link" target={"_blank"} href={appName.link}>VIEW</a>
-                </li> 
-              )} 
-            </ul>
-        </div>
-      </li>
+      <div>
+        {title.apps.length > 0 && 
+        <li className="list-item">
+          <div className="app-item-wraper">
+            <h3 className="list-subtitle">{title.subtitle}</h3>
+              <h2 className="list-title">{title.title}</h2>
+              {title.heroimage && 
+                <div className="list-image"><img src={title.heroimage} /></div>
+              }
+              <ul>
+                { title.apps.map(appName => 
+                  <li className="app-item">
+                      <picture className="app-image">
+                        <source srcSet={appName.picture} media={'(min-width: 1069px)'} />
+                        <img src={appName.avatar} />
+                      </picture>
+                      <div className="app-info">
+                        <div className="app-title">{appName.title}</div>
+                        <div className="app-category">{appName.category}</div>
+                      </div>
+                      <a className="app-link" target={"_blank"} href={appName.link}>VIEW</a>
+                  </li> 
+                )} 
+              </ul>
+            </div>
+          </li>
+        }
+      </div>
     );
     return (
       <ul className="list-container">{listItems}</ul>
