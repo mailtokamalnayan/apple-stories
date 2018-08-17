3cbac1917a4697175e2a3b7bf1d8bc28bced2962
mailtokamalnayan <mailtokamalnayan@gmail.com>
2018-08-17 22:34:33 +0800

    Import links from Google Spreadsheet

--- src/scraper.js
+++ src/scraper.js
@@ -1,16 +1,46 @@
 const scrapeIt = require("scrape-it")
 var fs = require('fs');
 
-const links = ['https://itunes.apple.com/in/story/id1314351295','https://itunes.apple.com/in/story/id1286988230','https://itunes.apple.com/in/story/id1278619470','https://itunes.apple.com/in/story/id1353737320','https://itunes.apple.com/in/story/id1296536229','https://itunes.apple.com/in/story/id1337185897','https://itunes.apple.com/in/story/id1301964484','https://itunes.apple.com/in/story/id1314402775','https://itunes.apple.com/in/story/id1299409355'];
+var GoogleSpreadsheet = require('google-spreadsheet');
+var async = require('async');
+ 
+var doc = new GoogleSpreadsheet('1aD1Hvg3kvDm4lApbTJIelEgBbIxJIfM-U4jabniJCjw');
+var sheet;
 
-console.log("Something is happening!")
+const linksFromSheet = [];
 
 var obj = {
-    appList: []
- };
+  appList: []
+};
 
-function getTitles() {
-    const titles = links.map(function(links) {
+async.series([
+  function setAuth(step) {
+
+    var creds = require('./creds.json');
+ 
+    doc.useServiceAccountAuth(creds, step);
+  },
+  function getInfoAndWorksheets(step) {
+    doc.getInfo(function(err, info) {
+      console.log('Loaded doc: '+info.title+' by '+info.author.email);
+      sheet = info.worksheets[0];
+      console.log('sheet 1: '+sheet.title+' '+sheet.rowCount+'x'+sheet.colCount);
+      step();
+    });
+  },
+  function workingWithCells(step) {
+    sheet.getCells({
+      'min-row': 1,
+      'max-row': 300,
+      'return-empty': false
+    }, function(err, cells) {
+      cells.map((val) => console.log(val.value));
+      cells.map((val) => linksFromSheet.push(val.value));
+      step();
+    });
+  },
+  function getTitles() {
+    const titles = linksFromSheet.map(function(links) {
         scrapeIt(links, {
         title: ".story-header__headline",
         subtitle: ".story-header__subhead",
@@ -38,14 +68,20 @@ function getTitles() {
           }
       }
       }).then(({ data, response }) => {
-          console.log(`Status Code: ${response.statusCode}`)
-          console.log(data)
+          // console.log(`Status Code: ${response.statusCode}`)
+          // console.log(data)
           obj.appList.push(data);
           var json = JSON.stringify(obj);
           fs.writeFile('scraped.json', json, 'utf8');
       });
     })
   }
+], function(err){
+    if( err ) {
+      console.log('Error: '+err);
+    }
+});
 
-getTitles();
+const links = ['https://itunes.apple.com/in/story/id1314351295','https://itunes.apple.com/in/story/id1286988230','https://itunes.apple.com/in/story/id1278619470','https://itunes.apple.com/in/story/id1353737320','https://itunes.apple.com/in/story/id1296536229','https://itunes.apple.com/in/story/id1337185897','https://itunes.apple.com/in/story/id1301964484','https://itunes.apple.com/in/story/id1314402775','https://itunes.apple.com/in/story/id1299409355'];
 
+console.log("Something is happening!")
