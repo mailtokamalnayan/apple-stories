2707734698c849e2d0d8a7f7370a6360c9202631
mailtokamalnayan <mailtokamalnayan@gmail.com>
2018-08-23 18:25:02 +0800

    Shift Pagination to bottom

--- src/App.js
+++ src/App.js
@@ -38,37 +38,28 @@ class App extends Component {
     
     if ( totalBlockNumber === 0) return null;
     
-    const headerClass = [
-      "text-dark py-2 pr-4 m-0",
-      currentPage ? "border-gray border-right" : ""
-    ]
-    .join(" ")
-    .trim();
-
     return (
       <div className="App">
         <h1 className="page-title">App Store App Collections</h1>
-        <h2 className={headerClass}>
-        <strong className="text-secondary">{totalBlockNumber}</strong>{" "}
-          App Collections
-        </h2>
-        {currentPage && (
-                <span className="current-page d-inline-block h-100 pl-4 text-secondary">
-                  Page <span className="font-weight-bold">{currentPage}</span> /{" "}
-                  <span className="font-weight-bold">{totalPages}</span>
-                </span>
-              )}
-        <Pagination
-          totalRecords={totalBlockNumber}
-          pageLimit={20}
-          pageNeighbours={0}
-          onPageChanged={this.onPageChanged}
-        />
         <ul className="list-container">
           {currentBlocks.map(collection => (
             <StoriesLink appList={collection} />
           ))}
           </ul>
+          <Pagination
+            totalRecords={totalBlockNumber}
+            pageLimit={20}
+            pageNeighbours={0}
+            onPageChanged={this.onPageChanged}
+          />
+          {currentPage && (
+            <span className="current-page d-inline-block h-100 pl-4 text-secondary">
+              Page <span className="font-weight-bold">{currentPage}</span> /{" "}
+              <span className="font-weight-bold">{totalPages}</span>
+            </span>
+          )}
+          <strong className="text-secondary">{totalBlockNumber}</strong>{" "}
+          App Collections
       </div>
     )
   }
